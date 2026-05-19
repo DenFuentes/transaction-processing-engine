f2edc0c02df19650dafcc8f0add42b9824eac1c5
Denis Fuentes <denisfuentes910@gmail.com>
2026-05-18 20:50:08 -0500

    implemented createTransaction with timestamp and ID generation

---
+++ src/transaction.cpp
@@ -0,0 +1,26 @@
+#include "transaction.h"
+#include <ctime>
+
+
+std::string getCurrentTimestamp() {
+    time_t now = time(0);
+    std::string timestamp = std::ctime(&now);
+    return timestamp;
+}
+
+std::string generateTransactionId() {
+    //todo
+    std::string transactionId = "TXN" + std::to_string(time(0));
+    return transactionId;
+}
+
+Transaction createTransaction(std::string cardNumber, double amount, std::string merchantId) {
+    Transaction transaction;
+    transaction.transactionId = generateTransactionId();
+    transaction.cardNumber = cardNumber;
+    transaction.amount = amount;
+    transaction.merchantId = merchantId;
+    transaction.timestamp = getCurrentTimestamp();
+    transaction.status = APPROVED; // Default status, can be updated later
+    return transaction;
+}
